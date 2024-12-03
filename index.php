34f84a7cd5a981610c7caa561f99105f4b52080a
indeedphp <ntsiti@gmail.com>
2024-12-03 12:30:14 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>45364536546432222222565622</p>
+<?php
+    phpinfo();
+ php?>
+
+
+</body>
+</html>
